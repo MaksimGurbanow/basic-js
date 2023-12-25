a49798665ba12db3699ff99b997ed82e7a56982d
maksim <maksim20051708@gmail.com>
2023-12-26 01:25:52 +0300

    add: solve

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,8 +16,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
   const {
     repeatTimes = 1,
     separator = '+',
